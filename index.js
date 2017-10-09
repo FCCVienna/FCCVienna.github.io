3fba311a9103c759acb5d5404d799c4bf293dc62
Nadezxda <nadja@malinkakalinka>
2017-10-09 11:48:57 +0200

    Add broken image method, fix typo and beautify

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
   $(".parallax").parallax();
-  var options = [
-      {selector: '#staggeredList', offset: 300, callback: function(el) {
-        Materialize.showStaggeredList($(el));
-      } },
-    ];
-    Materialize.scrollFire(options);
+  var options = [{
+    selector: '#staggeredList',
+    offset: 300,
+    callback: function(el) {
+      Materialize.showStaggeredList($(el));
+    }
+  }, ];
+  Materialize.scrollFire(options);
 });
+
+function brokenImage(link, id) {
+  var image = document.getElementById(id + 'new');
+  var old = document.getElementById(id);
+  sub = "<img src='" + link + "' alt='background picture showing a view of Vienna' />";
+  image.innerHTML = sub;
+  image.style.display = "inline";
+  old.style.display = "none";
+};
