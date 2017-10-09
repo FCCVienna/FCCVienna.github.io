88975bef9f60ed00ebd4a2783f4dc8114bd254ea
Nadezxda <nadja@malinkakalinka>
2017-10-09 11:48:57 +0200

    Add broken image method, fix typo and beautify

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
   $(".parallax").parallax();
-  var options = [
-      {selector: '#staggeredList', offset: 300, callback: function(el) {
-        Materialize.showStaggeredList($(el));
-      } },
-    ];
-    Materialize.scrollFire(options);
+  var options = [{
+    selector: '#staggeredList',
+    offset: 300,
+    callback: function(el) {
+      Materialize.showStaggeredList($(el));
+    }
+  }, ];
+  Materialize.scrollFire(options);
 });
